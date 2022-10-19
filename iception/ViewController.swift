ac2dc938d31009106bac0d2e6b068ccbae2475a5
fabianbohn <Mystek.fb@googlemail.com>
2022-10-19 15:49:12 +0200

    star rating iplementet

--- iception/ViewController.swift
+++ iception/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var starButtons: [UIButton]!
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    
+    for button in starButtons {
+    if button.tag <= sender.tag {
+        button.setImage(.init(systemName: "star.fill"), for: .normal)
+    } else {
+    button.setImage(.init(systemName: "star"), for: .normal)
+    }
+    }
+    }
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
